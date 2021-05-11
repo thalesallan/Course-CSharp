63eba006f96e28dd4a0b4f8b501707809a0c0bef
thalesallan <trabalhecomthales@gmail.com>
2021-05-11 00:28:17 -0300

    Criação do método Area na class Triangulo

--- Course/Program.cs
+++ Course/Program.cs
@@ -19,10 +19,10 @@ namespace Course
             y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double p = (x.A + x.B + x.C) / 2.0;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-            p = (y.A + y.B + y.C) / 2.0;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
             if (areaX > areaY)
